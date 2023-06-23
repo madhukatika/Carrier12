f4caa075aa98691d8878104eb05b9c60461918aa
Katika <Madhavi.Katika@mind.motherson.com>
2023-06-23 15:00:25 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,30 @@
+
+
+import './App.css';
+
+import SearchComponent from './Components/SearchData';
+import ChartComponent from './Components/charts/index';
+
+
+
+// import allFiles from '../src/json_data/merged_file.json'
+
+
+function App() {
+  return (
+    <>
+
+
+ <SearchComponent/>
+ <ChartComponent/>
+{/* <Datepicker/> */}
+
+
+
+ 
+  </>
+   
+  );
+}
+
+export default App;
